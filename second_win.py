ad045d364be3af96663f0bfad5302c51e44829e0
LKTSHUKUROV <timur5109sh@mail.ru>
2024-01-13 22:25:49 +0300

    добавил методы

--- second_win.py
+++ second_win.py
@@ -1,4 +1,6 @@
-# напиши здесь код для второго экрана приложения
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout
 class TestWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -6,3 +8,29 @@ class TestWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.text_name = QLabel(txt_name)
+        self.text_age = QLabel(txt_age)
+        self.text_test1 = QLabel(txt_test1)
+        self.text_test2 = QLabel(txt_test2)
+        self.text_test3 = QLabel(txt_test3)
+        self.text_timer = QLabel('')
+
+        self.start_test1 = QPushButton(txt_starttest1)
+        self.start_test2 = QPushButton(txt_starttest2)
+        self.start_test3 = QPushButton(txt_starttest3)
+        self.finall = QPushButton(txt_finalwin)
+
+        
+
+
+        
+
+
+
+        self.h_line = 
